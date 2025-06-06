fba88d25cbf3c88a87ceea1d7e27da8ee3f0b74b
Lava <wereop123@gmail.com>
2025-06-06 16:34:32 +0800

    add recordcard & constant

---
+++ lib/model/recordcard.dart
@@ -0,0 +1,12 @@
+class RecordCard {
+  final DateTime dateTime;
+  final String name;
+  final int price;
+
+  RecordCard({
+    required this.dateTime,
+    required this.name,
+    required this.price,
+
+});
+}
