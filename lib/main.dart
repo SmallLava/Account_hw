18c3b621906cc3a893d874cdc55df98cbe6b16ab
1234 <rau12341234@gmail.com>
2025-06-06 17:46:17 +0800

    fix: main error

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 
 
-import './view/Transaction.dart';
-void main() => runApp(MaterialApp(home: Transaction()));
 
 
-// void main() => runApp(MyApp());
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
